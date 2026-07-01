a6df5d2912af9afc82be8373c242e01566bebf8f
TomaszSulzycki0 <sultomek@gmail.com>
2026-07-01 14:01:30 +0200

    feat: input file handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,48 @@
+#include<vector>
 #include<iostream>
+#include<regex>
+#include<string>
+#include<filesystem>
 
-int main(std::size_t argc, char** argv)
+constexpr int num_args_for_default_usage = 2;
+constexpr int num_args_for_description = 1;
+
+void programDesc();
+bool hasCorrectExtension(const std::string& filename, const std::string& expected_ext);
+
+int main(int argc, char** argv)
 {
-    for(std::size_t i {}; i < argc; ++i)
+    switch (argc)
     {
-        std::printf(argv[i]);
-        std::printf("\n");
+    case num_args_for_description:
+        programDesc();
+        return 0;
+    case num_args_for_default_usage:
+        
+        break;
+    default:
+        std::cerr << "Invalid number of parameters passed to interpreter: " << argc - 1 << std::endl;
+        return -1;
     }
 
-    std::printf("test.\n");
-    int in = 0;
-    std::cin >> in;
+    std::string input_filename = argv[1];
+
+    if(!hasCorrectExtension(input_filename, ".ipl"))
+    {
+        std::cerr << "Invalid input file extension, expecting .ipl" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Running: " << input_filename << std::endl;
+
+}
+
+void programDesc()
+{
+    std::cout << "-----------------------\nInterpreted Programming Language\nPersonal Project\nUsage command: ilp filename.ilp\n-----------------------" << std::endl;
+}
+
+bool hasCorrectExtension(const std::string& filename, const std::string& expected_ext) 
+{
+    return std::filesystem::path(filename).extension() == expected_ext;
 }
