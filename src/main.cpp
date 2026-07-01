8e67997f8552af76aabc753eb2a9ca29f14d89bc
TomaszSulzycki0 <sultomek@gmail.com>
2026-07-01 22:25:00 +0200

    feat: splitting code into statements

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ constexpr int num_args_for_description = 1;
 void programDesc();
 bool hasCorrectExtension(const std::string& filename, const std::string& expected_ext);
 std::string readFileToString(const std::string& filename);
+std::vector<std::string> getStatements(const std::string& code);
 
 int main(int argc, char** argv)
 {
@@ -40,9 +41,15 @@ int main(int argc, char** argv)
     try
     {
         auto file_content = readFileToString(input_filename);
-        std::cout << file_content;
+        auto statements = getStatements(file_content);
 
-        Scope global_scope {};
+        std::cout << "Num statements: " << statements.size() << std::endl;
+
+        for(int i = 0; i < statements.size(); ++i)
+        {
+            std::cout << "Statement " << i << " " << statements[i] << std::endl;
+        } 
+        
     }
     catch(const std::exception& e)
     {
@@ -72,4 +79,41 @@ std::string readFileToString(const std::string& filename)
     std::ostringstream buffer;
     buffer << file_stream.rdbuf();
     return buffer.str();
+}
+
+std::vector<std::string> getStatements(const std::string& code)
+{
+    std::vector<std::string> output;
+    std::size_t pos {};
+    auto code_size = code.size();
+    
+    for(std::size_t i {}; i < code_size; ++i)
+    {
+        auto semicolon_pos = code.find(';', pos);
+
+        // No next semicolon
+        if(semicolon_pos  == std::string::npos)
+        {
+            break;
+        }
+
+        // Does not include semicolon in statement
+        auto raw_substring = code.substr(pos, semicolon_pos - pos);
+        auto first_char = raw_substring.find_first_not_of(" \n");
+
+        pos = semicolon_pos + 1; 
+
+        // Edge cases
+        // Handles multiple semicolons, white spaces, new lines
+        // fix: WILL NOT THROW IF THE LAST LINE OF CODE WITHOUT SEMICOLON!
+        if(raw_substring.empty() || first_char  == std::string::npos)
+        {
+            continue;
+        }
+
+        auto statement = raw_substring.substr(first_char);
+        output.push_back(statement);
+    }
+
+    return output;
 }
