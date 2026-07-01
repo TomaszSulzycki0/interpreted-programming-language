e829d393c367dec6bbfd7745bc072cb6ad4ed809
TomaszSulzycki0 <sultomek@gmail.com>
2026-07-01 14:55:02 +0200

    feat: reading input file

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 #include<regex>
 #include<string>
 #include<filesystem>
+#include<fstream>
+#include<stdexcept>
 
 constexpr int num_args_for_default_usage = 2;
 constexpr int num_args_for_description = 1;
 
 void programDesc();
 bool hasCorrectExtension(const std::string& filename, const std::string& expected_ext);
+std::string readFileToString(const std::string& filename);
 
 int main(int argc, char** argv)
 {
@@ -18,7 +21,6 @@ int main(int argc, char** argv)
         programDesc();
         return 0;
     case num_args_for_default_usage:
-        
         break;
     default:
         std::cerr << "Invalid number of parameters passed to interpreter: " << argc - 1 << std::endl;
@@ -29,11 +31,20 @@ int main(int argc, char** argv)
 
     if(!hasCorrectExtension(input_filename, ".ipl"))
     {
-        std::cerr << "Invalid input file extension, expecting .ipl" << std::endl;
+        std::cerr << "Invalid input file extension. Expecting .ipl" << std::endl;
         return -1;
     }
 
-    std::cout << "Running: " << input_filename << std::endl;
+    try
+    {
+        auto file_content = readFileToString(input_filename);
+        std::cout << file_content;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
 
 }
 
@@ -45,4 +56,16 @@ void programDesc()
 bool hasCorrectExtension(const std::string& filename, const std::string& expected_ext) 
 {
     return std::filesystem::path(filename).extension() == expected_ext;
+}
+
+std::string readFileToString(const std::string& filename) 
+{
+    std::ifstream file_stream(filename);
+    if (!file_stream.is_open()) {
+        throw std::runtime_error("Unable to access file: " + filename);
+    }
+    
+    std::ostringstream buffer;
+    buffer << file_stream.rdbuf();
+    return buffer.str();
 }
