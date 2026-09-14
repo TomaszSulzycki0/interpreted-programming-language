4b52c43ab8ee5c05c46524161e35b104d66b1f92
TomaszSulzycki0 <sultomek@gmail.com>
2026-09-14 18:32:19 +0200

    feat: typechecker assignment tests

---
+++ tests/typechecker_assignment_test.cpp
@@ -0,0 +1,95 @@
+#include <gtest/gtest.h>
+
+#include"typeChecker.hpp"
+#include"visitors.hpp"
+#include"abstractNodes.hpp"
+#include"parser.hpp"
+#include"scope.hpp"
+
+class TypecheckerAssignmentTest : public ::testing::Test
+{
+protected:
+    std::shared_ptr<SemanticScope> main_semantic_scope = std::make_shared<SemanticScope>();
+
+    TypeChecker typechecker { main_semantic_scope };
+
+    bool typechecks(const std::string& code)
+    {
+        Parser parser { code };
+        parser.tokenizeProgram();
+
+        const auto ast = parser.parseProgram(PARSING_MODE::DEFAULT);
+
+        EXPECT_TRUE(parser.isASTExecutable());
+
+        typechecker.run_check(ast);
+
+        return typechecker.isASTWellTyped();
+    }
+};
+
+TEST_F(TypecheckerAssignmentTest, AllowsAssignmentWithGoodFunctionCall)
+{
+    EXPECT_TRUE(typechecks(R"(
+        fn rBool() -> bool { return true; } 
+        fn rInt() -> int { return 1; } 
+        fn rFloat() -> float { return 1.0; } 
+        fn rDouble() -> double { return 1.0; } 
+        fn rString() -> string { return "true"; } 
+
+        bool b; 
+        int x; 
+        float y; 
+        double d; 
+        string s;
+
+        b = rBool(); 
+        x = rInt(); 
+        y = rFloat(); 
+        d = rDouble(); 
+        s = rString(); 
+    )"));
+}
+
+TEST_F(TypecheckerAssignmentTest, AllowsAssignmentWithImplicitTypeUpcasting)
+{
+    EXPECT_TRUE(typechecks(R"(
+
+        bool b; 
+        int x; 
+        float y; 
+        double d; 
+        string s;
+
+        x = b;
+
+        y = b;
+        y = x;
+
+        d = b;
+        d = x;
+        d = y;
+    )"));
+}
+
+TEST_F(TypecheckerAssignmentTest, ForbidsAssignmentWithImplicitTypeDowncasting)
+{
+    EXPECT_FALSE(typechecks(R"(
+ 
+        float y; 
+        double d; 
+
+        y = d;
+    )"));
+}
+
+TEST_F(TypecheckerAssignmentTest, ForbidsBadAssignmentWithUncastableValue)
+{
+    EXPECT_FALSE(typechecks(R"(
+ 
+        int x; 
+        string s = "aa"; 
+
+        x = s;
+    )"));
+}
